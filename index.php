591f789efe99bf0ee6a96c544c4b1dc544e3845f
Landon Conover <landon.conover@uvu.edu>
2012-11-01 11:41:12 -0600

    Added ForkMe banner

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <!--
   Created using jsbin.com
-  Source can be edited via http://jsbin.com/welcome/41227/edit
+  Source can be forked at github.com/landonconover/Assign5
 -->
 <head>
     <meta charset="utf-8">
@@ -13,8 +13,7 @@
     <link rel="stylesheet" href="css/myStyle.css">
 </head>
 <body>
-    <!-- Script 7.3 - words.html -->
-    <form action="#" method="post" id="theForm">
+    <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>    <form action="#" method="post" id="theForm">
         <fieldset><legend>Words To Be Sorted!</legend>
             <p>Enter some words to be sorted in the input below. Separate each word with a space. Click the button and you'll see a case-insensitive sort of those words.</p>
             <div><label for="words">Words</label><input type="text" name="words" id="words" required></div>
