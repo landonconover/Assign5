3a85e3556e7e9da0ba449f14da253cb63f40ecf6
Landon Conover <landon.conover@uvu.edu>
2012-11-01 11:00:04 -0600

    Inital commit

---
+++ js/words.js
@@ -0,0 +1,55 @@
+
+// words.js
+// This script defines a function for sorting words in a case-insenstive manner.
+
+// Define the $() shortcut function below:
+ 
+
+
+
+
+	// End of $ function.
+
+// Function for setting text of an element:
+function setText(elementId, message) {
+    'use strict';
+    
+ 
+	
+	
+	
+	
+	
+	
+	// End of main IF.
+} // End of setText() function.
+
+// This function sorts a list of words.
+// The function takes one argument, a string.
+function sortWords(max) {
+    'use strict';
+	
+    // Get the words:
+    // One line from Step 4 on page 252 goes here
+    
+    // Convert the string to an array:
+    // One line from Step 5 on page 252 goes here
+    
+    // Sort the words:
+    // The code in Step 6 on page 252 goes here - all of it
+    
+    
+    
+    // Send the output to the page:
+    // One line from Step 7 on page 253 goes here
+        
+    // Return false to prevent submission:
+    return false;
+
+} // End of sortWords() function.
+
+function init() {
+    'use strict';
+    // One line from step 9 on page 253 goes here
+} // End of init() function.
+window.onload = init;
