19b6c81e45aa89184ee0e44269ca3f3964b25fc8
Landon Conover <landon.conover@uvu.edu>
2012-11-01 11:36:01 -0600

    Added punctuation to the title.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 -->
 <head>
     <meta charset="utf-8">
-    <title>Words To Be Sorted</title>
+    <title>Words To Be Sorted!</title>
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
@@ -15,7 +15,7 @@
 <body>
     <!-- Script 7.3 - words.html -->
     <form action="#" method="post" id="theForm">
-        <fieldset><legend>Words To Be Sorted</legend>
+        <fieldset><legend>Words To Be Sorted!</legend>
             <p>Enter some words to be sorted in the input below. Separate each word with a space. Click the button and you'll see a case-insensitive sort of those words.</p>
             <div><label for="words">Words</label><input type="text" name="words" id="words" required></div>
             <input type="submit" value="Sort!" id="submit">
