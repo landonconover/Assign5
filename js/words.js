e3a858ea98ce473a51545c2ff74acdd541031e55
Landon Conover <landon.conover@uvu.edu>
2012-11-08 14:10:40 -0700

    Finished with the assignment. The word sorter works!

--- js/words.js
+++ js/words.js
@@ -4,44 +4,49 @@
 
 // Define the $() shortcut function below:
  
-
-
-
-
-	// End of $ function.
+function $(elId){
+    if (typeof elId != 'undefined'){
+      return document.getElementById(elId);  
+    }
+} // End of $ function.
 
 // Function for setting text of an element:
 function setText(elementId, message) {
     'use strict';
-    
- 
-	
-	
-	
-	
-	
-	
-	// End of main IF.
+    if( ( typeof elementId == 'string') && ( typeof message == 'string' ) ){
+
+        var output = $(elementId);
+
+        if (output.textContent !== undefined) {
+            output.textContent = message;
+        } else {
+            output.innerText = message;
+        }
+    } // End of main IF.
 } // End of setText() function.
 
 // This function sorts a list of words.
 // The function takes one argument, a string.
 function sortWords(max) {
     'use strict';
-	
+
     // Get the words:
     // One line from Step 4 on page 252 goes here
+     var words = $('words').value;
     
     // Convert the string to an array:
     // One line from Step 5 on page 252 goes here
-    
+    words = words.split(' ');
+
     // Sort the words:
     // The code in Step 6 on page 252 goes here - all of it
-    
-    
+    var sorted = words.map(function(value) {
+        return value.toLowerCase();
+    }).sort();      
     
     // Send the output to the page:
     // One line from Step 7 on page 253 goes here
+    setText('output', sorted.join(', '));
         
     // Return false to prevent submission:
     return false;
@@ -51,5 +56,6 @@ function sortWords(max) {
 function init() {
     'use strict';
     // One line from step 9 on page 253 goes here
+     $('theForm').onsubmit = sortWords;
 } // End of init() function.
 window.onload = init;
